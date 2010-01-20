c86a4b711315fc5a7088ee961c1ea46c3477701b
agentzh (章亦春) <agentzh@gmail.com>
2010-01-20 18:05:41 +0800

    fixed syntax error in init-db.sql

--- demo/Blog/misc/mysql/init-db.sql
+++ demo/Blog/misc/mysql/init-db.sql
@@ -1,4 +1,5 @@
 -- Blog posts
+drop table if exists posts;
 create table posts (
     id serial,
     title varchar(128) not null,
@@ -10,13 +11,15 @@ create table posts (
 );
 
 -- Blog comments
+drop table if exists comments;
 create table comments (
     id serial,
     sender varchar(64) not null,
     email varchar(64) not null,
-    url carchar(1024),
+    url varchar(1024),
     body text,
-    created timestamp(0) deafult now() not null,
+    created timestamp(0) default now() not null,
+    post bigint not null,
     foreign key (post) references posts(id)
 );
 
