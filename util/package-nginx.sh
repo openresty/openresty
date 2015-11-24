83eeb14f6ccbbfb81adfe3a0085c3950ee619c47
Yichun Zhang (agentzh) <agentzh@gmail.com>
2015-11-24 22:37:40 +0800

    upgraded the nginx core to 1.9.7.

--- util/package-nginx.sh
+++ util/package-nginx.sh
@@ -13,6 +13,10 @@ if [ ! -d nginx/.git ]; then
     exit 1
 fi
 
+cd nginx || exit 1
+git pull --force || exit 1
+cd .. || exit 1
+
 rm -rf nginx-$ver
 rm -f nginx-$ver.tar.gz
 git clone nginx nginx-$ver || exit 1
@@ -26,5 +30,5 @@ rm -rf src/mysql || exit 1
 cd ..
 tar czvf nginx-$ver.tar.gz nginx-$ver || exit 1
 ls -lh ./nginx-$ver.tar.gz
-ls -lh ~/work/nginx-$ver.tar.gz
+#ls -lh ~/work/nginx-$ver.tar.gz
 scp nginx-$ver.tar.gz agentzh.org:~/www/agentzh/misc/nginx/
