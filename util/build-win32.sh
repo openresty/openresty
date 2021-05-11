fcac763f2231dbee22bffbd4e6172d7eae45ee4d
Johnny Wang <wangjiahao@openresty.com>
2021-05-11 23:07:37 +0800

    win32: upgraded openssl to 1.1.1k. (#730)

--- util/build-win32.sh
+++ util/build-win32.sh
@@ -2,10 +2,10 @@
 
 PCRE=pcre-8.44
 ZLIB=zlib-1.2.11
-OPENSSL=openssl-1.1.1g
+OPENSSL=openssl-1.1.1k
 JOBS=12
 
-# wget https://www.openssl.org/source/openssl-1.1.1g.tar.gz
+# wget https://www.openssl.org/source/openssl-1.1.1k.tar.gz
 # wget http://zlib.net/zlib-1.2.11.tar.gz
 # wget https://ftp.pcre.org/pub/pcre/pcre-8.44.tar.gz
 
